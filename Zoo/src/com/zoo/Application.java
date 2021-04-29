76da56c7660e055e9357a64d30d7fc85d56f795c
kallard <kallard@student.21-school.ru>
2021-04-29 09:47:39 +0300

    tests in main created

--- Zoo/src/com/zoo/Application.java
+++ Zoo/src/com/zoo/Application.java
@@ -7,34 +7,76 @@ import com.zoo.exception.tooOldException;
 public class Application {
 
     public static void main(String[] args) throws tooOldException {
+        var zoo = new Zoo();
+        var enclosureLemurs = new Enclosure("Лемуры");
+        zoo.addEnclosure(enclosureLemurs);
+
+        try {
+            var lemur = new Lemur("Лемурка", 16);
+
+            try {
+                enclosureLemurs.addAnimal(lemur);
+                enclosureLemurs.addAnimal(lemur);
+            } catch (IllegalArgumentException e) {
+                System.err.println("Ошибка при наполнении вальера лемурами: " + e.getMessage());
+            }
+
+            lemur.raiseTail();
+            lemur.collectAvocados();
+            try {
+                lemur.move(125); }
+            catch (tooOldException e) {
+                System.err.println(e.getMessage());
+            }
+            lemur.play("прятки");
+        }
+        catch (IllegalArgumentException e) {
+            System.err.println(e.getMessage());
+        }
+
+
+        var enclosureCats = new Enclosure("Кошачьи");
+        zoo.addEnclosure(enclosureCats);
+
         try {
             var cat = new Cat("Мурка", 10, "розовый");
-            cat.setName("Гав");
-            System.out.println(cat.getName());
-            System.out.println(cat.getColor());
             cat.say();
             cat.catchMice();
             cat.pet();
             try {
                 cat.move(); }
             catch (tooOldException e) {
-                System.out.println(e.getMessage());
+                System.err.println(e.getMessage());
+            }
+            enclosureCats.addAnimal(cat);
+        }
+        catch (IllegalArgumentException e) {
+            System.err.println(e.getMessage());
+        }
+
+        try {
+            var catMatilda = new Cat("Матильда", 4, "белый");
+            try { enclosureCats.addAnimal(catMatilda); }
+            catch (IllegalArgumentException e) {
+                System.err.println(e.getMessage());
+            }
+            var catIzolda = new Cat("Изольда", 11, "серый");
+            try { enclosureCats.addAnimal(catIzolda); }
+            catch (IllegalArgumentException e) {
+                System.err.println(e.getMessage());
+            }
+            var catZarapka = new Cat("Царапка", 5, "черный");
+            try { enclosureCats.addAnimal(catZarapka); }
+            catch (IllegalArgumentException e) {
+                System.err.println(e.getMessage());
             }
         }
         catch (IllegalArgumentException e) {
-            System.out.println(e.getMessage());
+            System.err.println(e.getMessage());
         }
-        var lemur = new Lemur("Manya", 16);
 
-        lemur.raiseTail();
-        lemur.collectAvocados();
-        try {
-            lemur.move(125); }
-        catch (tooOldException e) {
-            System.out.println(e.getMessage());
-        }
-        lemur.play("прятки");
-        var enclosure = new Enclosure();
+
+
 
     }
 }
